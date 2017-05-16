23556debfeb7ad40fae812ede11f049df73a47e6
Alexandre Gauthier <alexandre230@msn.com>
2017-05-16 18:09:55 +0200

    histogram: use manual implementation

--- src/detectSequence.c
+++ src/detectSequence.c
@@ -199,7 +199,476 @@ struct DetectThreadArgs
 
 };
 
-#ifdef OPENCV
+template <typename PIX>
+const PIX* getPixels(int x, int y, const RectI& bounds, int nComps, const PIX* pixelData)
+{
+    if ( ( x < bounds.x1 ) || ( x >= bounds.x2 ) || ( y < bounds.y1 ) || ( y >= bounds.y2 ) ) {
+        return NULL;
+    } else {
+        const unsigned char* ret = (const unsigned char*)pixelData;
+        if (!ret) {
+            return 0;
+        }
+
+        std::size_t compDataSize = sizeof(PIX) * nComps;
+        ret = ret + (std::size_t)( y - bounds.y1 ) * compDataSize * bounds.width()
+        + (std::size_t)( x - bounds.x1 ) * compDataSize;
+
+        return (const PIX*)ret;
+    }
+}
+
+template <typename PIX>
+PIX* getPixels(int x, int y, const RectI& bounds, int nComps, PIX* pixelData)
+{
+    if ( ( x < bounds.x1 ) || ( x >= bounds.x2 ) || ( y < bounds.y1 ) || ( y >= bounds.y2 ) ) {
+        return NULL;
+    } else {
+        unsigned char* ret = (unsigned char*)pixelData;
+        if (!ret) {
+            return 0;
+        }
+
+        std::size_t compDataSize = sizeof(PIX) * nComps;
+        ret = ret + (std::size_t)( y - bounds.y1 ) * compDataSize * bounds.width()
+        + (std::size_t)( x - bounds.x1 ) * compDataSize;
+
+        return (PIX*)ret;
+    }
+}
+
+template <typename SRCPIX, typename DSTPIX>
+static DSTPIX convertPixelDepth(SRCPIX pix);
+
+
+/// numvals should be 256 for byte, 65536 for 16-bits, etc.
+
+/// maps 0-(numvals-1) to 0.-1.
+template<int numvals>
+float
+intToFloat(int value)
+{
+    return value / (float)(numvals - 1);
+}
+
+/// maps 0.-1. to 0-(numvals-1)
+template<int numvals>
+int
+floatToInt(float value)
+{
+    if (value <= 0) {
+        return 0;
+    } else if (value >= 1.) {
+        return numvals - 1;
+    }
+
+    return value * (numvals - 1) + 0.5;
+}
+
+///explicit template instantiations
+
+template <>
+float
+convertPixelDepth(unsigned char pix)
+{
+    return intToFloat<256>(pix);
+}
+
+template <>
+unsigned short
+convertPixelDepth(unsigned char pix)
+{
+    // 0x01 -> 0x0101, 0x02 -> 0x0202, ..., 0xff -> 0xffff
+    return (unsigned short)( (pix << 8) + pix );
+}
+
+template <>
+unsigned char
+convertPixelDepth(unsigned char pix)
+{
+    return pix;
+}
+
+template <>
+unsigned char
+convertPixelDepth(unsigned short pix)
+{
+    // the following is from ImageMagick's quantum.h
+    return (unsigned char)( ( (pix + 128UL) - ( (pix + 128UL) >> 8 ) ) >> 8 );
+}
+
+template <>
+float
+convertPixelDepth(unsigned short pix)
+{
+    return intToFloat<65536>(pix);
+}
+
+template <>
+unsigned short
+convertPixelDepth(unsigned short pix)
+{
+    return pix;
+}
+
+template <>
+unsigned char
+convertPixelDepth(float pix)
+{
+    return (unsigned char)floatToInt<256>(pix);
+}
+
+template <>
+unsigned short
+convertPixelDepth(float pix)
+{
+    return (unsigned short)floatToInt<65536>(pix);
+}
+
+template <>
+float
+convertPixelDepth(float pix)
+{
+    return pix;
+}
+
+// r,g,b values are from 0 to 1
+// h = [0,1.], s = [0,1], v = [0,1]
+//		if s == 0, then h = 0 (undefined)
+void
+rgb_to_hsv(float r,
+           float g,
+           float b,
+           float *h,
+           float *s,
+           float *v )
+{
+    float min = std::min(std::min(r, g), b);
+    float max = std::max(std::max(r, g), b);
+
+    *v = max;                               // v
+
+    float delta = max - min;
+
+    if (max != 0.) {
+        *s = delta / max;                       // s
+    } else {
+        // r = g = b = 0		// s = 0, v is undefined
+        *s = 0.f;
+        *h = 0.f;
+
+        return;
+    }
+
+    if (delta == 0.) {
+        *h = 0.f;                 // gray
+    } else if (r == max) {
+        *h = (g - b) / delta;                       // between yellow & magenta
+    } else if (g == max) {
+        *h = 2 + (b - r) / delta;                   // between cyan & yellow
+    } else {
+        *h = 4 + (r - g) / delta;                   // between magenta & cyan
+    }
+    *h *= 1. / 6.;
+    if (*h < 0) {
+        *h += 1.;
+    }
+}
+
+enum BitDepthEnum
+{
+    eBitDepthByte,
+    eBitDepthShort,
+    eBitDepthFloat,
+};
+
+template <typename SRCPIX, int nComps, typename DSTPIX>
+void convertRGBToHSVInternal(const SRCPIX* srcPixelsData, const RectI& srcBounds, const RectI& roi, DSTPIX* dstPixelsData, const RectI& dstBounds)
+{
+
+    const SRCPIX* srcPixels = getPixels<SRCPIX>(roi.x1, roi.y1, srcBounds, nComps, srcPixelsData);
+    DSTPIX* dstPixels = getPixels<DSTPIX>(roi.x1, roi.y1, srcBounds, nComps, dstPixelsData);
+
+    for (int y = roi.y1; y < roi.y2; ++y) {
+        for (int x = roi.x1; x < roi.x2; ++x, srcPixels += nComps, dstPixels += nComps) {
+
+            float tmpPix[3];
+            memset(tmpPix, 0, sizeof(float) * 3);
+
+            for (int c = 0; c < std::min(3,nComps); ++c) {
+                tmpPix[c] = convertPixelDepth<SRCPIX, float>(srcPixels[c]);
+            }
+            float hsv[3];
+            rgb_to_hsv(tmpPix[0], tmpPix[1], tmpPix[2], &hsv[0], &hsv[1], &hsv[2]);
+
+            for (int c = 0; c < std::min(3,nComps); ++c) {
+                dstPixels[c] = convertPixelDepth<float, DSTPIX>(hsv[c]);
+            }
+            for (int c = 3; c < nComps; ++c) {
+                dstPixels[c] = convertPixelDepth<SRCPIX, DSTPIX>(srcPixels[c]);
+            }
+
+        }
+        // Remove what was done on last iteration and go to the next line
+        srcPixels += (srcBounds.width() * nComps - roi.width() * nComps);
+        dstPixels += (dstBounds.width() * nComps - roi.width() * nComps);
+    }
+
+}
+
+template <typename SRCPIX, int nComps>
+void convertRGBToHSVForNComps(const SRCPIX* srcPixelsData, const RectI& srcBounds, const RectI& roi, BitDepthEnum dstDepth, void* dstPixelsData, const RectI& dstBounds)
+{
+    switch (dstDepth) {
+        case eBitDepthByte:
+            convertRGBToHSVInternal<SRCPIX, nComps, unsigned char>(srcPixelsData, srcBounds, roi, (unsigned char*)dstPixelsData, dstBounds);
+            break;
+        case eBitDepthShort:
+            convertRGBToHSVInternal<SRCPIX, nComps, unsigned short>(srcPixelsData, srcBounds, roi, (unsigned short*)dstPixelsData, dstBounds);
+            break;
+        case eBitDepthFloat:
+            convertRGBToHSVInternal<SRCPIX, nComps, float>(srcPixelsData, srcBounds, roi, (float*)dstPixelsData, dstBounds);
+            break;
+    }
+}
+
+template <typename SRCPIX>
+void convertRGBToHSVForSrcBitDepth(int nComps, const SRCPIX* srcPixelsData, const RectI& srcBounds, const RectI& roi, BitDepthEnum dstDepth, void* dstPixelsData, const RectI& dstBounds)
+{
+    switch (nComps) {
+        case 1:
+            convertRGBToHSVForNComps<SRCPIX, 1>(srcPixelsData, srcBounds, roi, dstDepth, dstPixelsData, dstBounds);
+            break;
+        case 2:
+            convertRGBToHSVForNComps<SRCPIX, 2>(srcPixelsData, srcBounds, roi, dstDepth, dstPixelsData, dstBounds);
+            break;
+        case 3:
+            convertRGBToHSVForNComps<SRCPIX, 3>(srcPixelsData, srcBounds, roi, dstDepth, dstPixelsData, dstBounds);
+            break;
+        case 4:
+            convertRGBToHSVForNComps<SRCPIX, 4>(srcPixelsData, srcBounds, roi, dstDepth, dstPixelsData, dstBounds);
+            break;
+        default:
+            assert(false);
+            break;
+    }
+}
+
+void convertRGBToHSV(int nComps, BitDepthEnum srcDepth, const void* srcPixelsData, const RectI& srcBounds, const RectI& roi, BitDepthEnum dstDepth, void* dstPixelsData, const RectI& dstBounds)
+{
+    switch (srcDepth) {
+        case eBitDepthByte:
+            convertRGBToHSVForSrcBitDepth<unsigned char>(nComps, (const unsigned char*)srcPixelsData, srcBounds, roi, dstDepth, dstPixelsData, dstBounds);
+            break;
+        case eBitDepthShort:
+            convertRGBToHSVForSrcBitDepth<unsigned short>(nComps, (const unsigned short*)srcPixelsData, srcBounds, roi, dstDepth, dstPixelsData, dstBounds);
+            break;
+        case eBitDepthFloat:
+            convertRGBToHSVForSrcBitDepth<float>(nComps, (const float*)srcPixelsData, srcBounds, roi, dstDepth, dstPixelsData, dstBounds);
+            break;
+    }
+}
+
+/**
+ * @brief Compute a N-Dimensional histogram where N <= srcNComps over the given region of interest.
+ * The number of bins across each dimension is given by histogramSizes which must be of size srcNComps
+ * If a dimension is of size 0, it indicates we are not interested in its histogram
+ **/
+template <typename PIX, int maxValue, int srcNComps>
+void computeHistogramInternal(const PIX* srcPixelsData, const RectI& srcBounds, const RectI& roi, const std::vector<int>& histogramSizes, std::vector<double>* histogram)
+{
+    assert(srcBounds.contains(roi));
+    assert(histogramSizes.size() == srcNComps);
+
+    int histStrides[srcNComps];
+    for (int i = 0; i < srcNComps; ++i) {
+        if (histogramSizes[i] > 0) {
+            histStrides[i] = 1;
+        } else {
+            histStrides[i] = 0;
+        }
+
+    }
+    std::size_t numElements = 1;
+    for (std::size_t i = 0; i < histogramSizes.size(); ++i) {
+        if (histogramSizes[i] > 0) {
+            numElements *= histogramSizes[i];
+            if (i > 0) {
+                for (std::size_t j = 0; j < i ; ++j) {
+                    histStrides[j] *= histogramSizes[j];
+                }
+            }
+        }
+    }
+    // Initialize to 0
+    histogram->resize(numElements, 0);
+
+
+
+
+    const PIX* srcPixels = getPixels(roi.x1, roi.y1, srcBounds, srcNComps, srcPixelsData);
+
+    for (int y = roi.y1; y < roi.y2; ++y) {
+        for (int x = roi.x1; x < roi.x2; ++x, srcPixels += srcNComps) {
+
+            int indices[srcNComps];
+            for (int c = 0; c < srcNComps; ++c) {
+                if (histogramSizes[c]) {
+                    // Convert to float and clamp to [0,1]
+                    float val = std::max(0.f,std::min(1.f,convertPixelDepth<PIX, float>(srcPixels[c])));
+
+                    // Map the normalized [0,1] value to a value in the range of the histogram size for that bin and round to nearest integer
+                    indices[c] = std::floor(val * (histogramSizes[c] - 1) + 0.5);
+                }
+            }
+            int index = 0;
+            for (int c = 0; c < srcNComps; ++c) {
+                if (histStrides[c]) {
+                    index += histStrides[c] * indices[c];
+                }
+            }
+            assert(index < (int)histogram->size());
+            (*histogram)[index] += 1;
+        }
+        // Remove what was done on last iteration and go to the next line
+        srcPixels += (srcBounds.width() * srcNComps - roi.width() * srcNComps);
+    }
+} // computeHistogramInternal
+
+template <typename PIX, int maxValue>
+void computeHistogramForNComps(int nComps, const PIX* srcPixelsData, const RectI& srcBounds, const RectI& roi, const std::vector<int>& histogramSizes, std::vector<double>* histogram)
+{
+    switch (nComps) {
+        case 1:
+            computeHistogramInternal<PIX, maxValue, 1>(srcPixelsData, srcBounds, roi, histogramSizes, histogram);
+            break;
+        case 2:
+            computeHistogramInternal<PIX, maxValue, 2>(srcPixelsData, srcBounds, roi, histogramSizes, histogram);
+            break;
+        case 3:
+            computeHistogramInternal<PIX, maxValue, 3>(srcPixelsData, srcBounds, roi, histogramSizes, histogram);
+            break;
+        case 4:
+            computeHistogramInternal<PIX, maxValue, 4>(srcPixelsData, srcBounds, roi, histogramSizes, histogram);
+            break;
+    }
+
+}
+
+void computeHistogram(int nComps, BitDepthEnum depth, const void* srcPixelsData, const RectI& srcBounds, const RectI& roi, const std::vector<int>& histogramSizes, std::vector<double>* histogram)
+{
+    switch (depth) {
+        case eBitDepthByte:
+            computeHistogramForNComps<unsigned char, 255>(nComps, (const unsigned char*)srcPixelsData, srcBounds, roi, histogramSizes, histogram);
+            break;
+        case eBitDepthShort:
+            computeHistogramForNComps<unsigned short, 65535>(nComps, (const unsigned short*)srcPixelsData, srcBounds, roi, histogramSizes, histogram);
+            break;
+        case eBitDepthFloat:
+            computeHistogramForNComps<float, 1>(nComps, (const float*)srcPixelsData, srcBounds, roi, histogramSizes, histogram);
+            break;
+    }
+
+}
+
+
+static void normalizeHistogram(std::vector<double>* histogram) {
+    double maxValue = 0.;
+    for (std::size_t i = 0; i < histogram->size(); ++i) {
+        maxValue = std::max((*histogram)[i], maxValue);
+    }
+
+    if (maxValue == 0) {
+        return;
+    }
+    for (std::size_t i = 0; i < histogram->size(); ++i) {
+        (*histogram)[i] /= maxValue;
+    }
+
+}
+template <typename T>
+class RamBuffer
+{
+    T* data;
+    std::size_t count;
+
+public:
+
+    RamBuffer()
+    : data(0)
+    , count(0)
+    {
+    }
+
+    ~RamBuffer()
+    {
+        clear();
+    }
+
+    T* getData()
+    {
+        return data;
+    }
+
+    const T* getData() const
+    {
+        return data;
+    }
+
+    void swap(RamBuffer& other)
+    {
+        std::swap(data, other.data);
+        std::swap(count, other.count);
+    }
+
+    std::size_t size() const
+    {
+        return count;
+    }
+
+    void resize(std::size_t size)
+    {
+        if (size == 0) {
+            return;
+        }
+        count = size;
+        if (data) {
+            free(data);
+            data = 0;
+        }
+        if (count == 0) {
+            return;
+        }
+        data = (T*)malloc( size * sizeof(T) );
+        if (!data) {
+            throw std::bad_alloc();
+        }
+    }
+
+    void resizeAndPreserve(std::size_t size)
+    {
+        if (size == 0 || size == count) {
+            return;
+        }
+        count = size;
+        data = (T*)realloc(data,size * sizeof(T));
+        if (!data) {
+            throw std::bad_alloc();
+        }
+    }
+
+    void clear()
+    {
+        count = 0;
+        if (data) {
+            free(data);
+            data = 0;
+        }
+    }
+
+
+};
+
 /**
  * @brief Computes H and S histograms for the given rectangle
  **/
@@ -212,6 +681,23 @@ static void computeHistograms(const RectI& roi, const image& image, std::vector<
     imageBounds.y2 = image.h;
     assert(imageBounds.contains(roi));
 
+
+    RamBuffer<unsigned char> hsvImage;
+    hsvImage.resize(roi.width() * roi.height() * image.c);
+
+    convertRGBToHSV(image.c, eBitDepthFloat, image.data, imageBounds, roi, eBitDepthByte, hsvImage.getData(), roi);
+
+    std::vector<int> histogramSizes(3);
+    histogramSizes[0] = HUE_HISTOGRAM_NUM_BINS;
+    histogramSizes[1] = SAT_HISTOGRAM_NUM_BINS;
+    histogramSizes[2] = 0;
+
+    computeHistogram(image.c, eBitDepthByte, hsvImage.getData(), roi, roi, histogramSizes, histogramOut);
+
+    normalizeHistogram(histogramOut);
+
+
+#if 0
 
 
     // Convert the image to a cv::Mat
@@ -243,6 +729,7 @@ static void computeHistograms(const RectI& roi, const image& image, std::vector<
         }
     }
 
+
     // Convert to HSV
     cv::cvtColor( rgbMat, hsvMat, cv::COLOR_BGR2HSV);
 
@@ -260,7 +747,7 @@ static void computeHistograms(const RectI& roi, const image& image, std::vector<
     cv::Mat hist;
 
     // Calculate the histograms for the HSV images
-    calcHist( &hsvMat, 1, channels, cv::Mat() /*mask*/, hist, 2, histSize, ranges, true /*uniform*/, false /*accumulate*/);
+    calcHist( &hsvMat, 1, channels, cv::Mat(), hist, 2, histSize, ranges, true, false);
     assert(hist.type() ==  CV_32F);
     // normalize between 0 and 1
     cv::normalize( hist, hist, 0, 1, cv::NORM_MINMAX, -1, cv::Mat() );
@@ -275,16 +762,23 @@ static void computeHistograms(const RectI& roi, const image& image, std::vector<
             ++histData;
         }
     }
+#endif
 
     // Write in output
 }
-#endif // #ifdef OPENCV
 
 void *detect_in_thread(void *ptr)
 {
 
     DetectThreadArgs* args = (DetectThreadArgs*)ptr;
     args->ok = true;
+
+    std::vector<double> test;
+    RectI rod;
+    rod.x1 = rod.y1 = 0;
+    rod.x2 = args->inputImage.w;
+    rod.y2 = args->inputImage.h;
+    computeHistograms(rod, args->inputImage, &test);
 
     float nms = .4;
 
